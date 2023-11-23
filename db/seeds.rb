3c372dea7daa0ace9f6e8d789ed88523356ad7db
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2023-11-23 18:43:51 +0100

    tested seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+suspect = Suspect.create(name: "Drako Malfoy")
