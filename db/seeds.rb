b369d90c9a72777308555795a967d5eb86616233
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2024-12-04 15:48:50 +0100

    fixed background images added new background image for mobile view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-suspect = Suspect.create(name: "Drako Malfoy")
